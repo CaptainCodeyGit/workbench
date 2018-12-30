5c60d73ae50c57327a7095dc85fe421297a74bd0
Shahiem Seymor <shahiemseymor@shahiems-mbp.home>
2018-12-30 12:00:59 +0100

    Add check if the extension table exists

--- migrations/2018_12_29_234810_add_workbench_table.php
+++ migrations/2018_12_29_234810_add_workbench_table.php
@@ -5,16 +5,19 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->create('captainc_extensions', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('author');
-            $table->text('description');
-            $table->string('icon')->nullable();
-            $table->string('icon_color')->nullable();
-            $table->string('icon_background')->nullable();
-            $table->timestamps();
-        });
+        if (!$schema->hasTable('captainc_extensions'))
+        {
+            $schema->create('captainc_extensions', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('author');
+                $table->text('description');
+                $table->string('icon')->nullable();
+                $table->string('icon_color')->nullable();
+                $table->string('icon_background')->nullable();
+                $table->timestamps();
+            });
+        }
     },
 
     'down' => function (Builder $schema) {
